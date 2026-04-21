01e3461f710d074c998920bbc90dd55a5824fda0
03-JS <pyjoso3vitaslim@gmail.com>
2026-04-21 18:36:40 +0200

    forgor this too, oops

--- LethalDebt/Patches/TimeOfDayPatches.cs
+++ LethalDebt/Patches/TimeOfDayPatches.cs
@@ -30,7 +30,7 @@ namespace LethalDebt.Patches
         [HarmonyPostfix]
         static void DisplayTip(TimeOfDay __instance)
         {
-            if (__instance.timesFulfilledQuota != Plugin.Instance.quotaDeadline.Value - 1) Utils.StartCoroutine(DisplayDebtReminder(__instance));
+            if (__instance.timesFulfilledQuota != Plugin.Instance.quotaDeadline.Value - 1 && TerminalPatches.terminal.groupCredits < 0) Utils.StartCoroutine(DisplayDebtReminder(__instance));
         }
         
         static IEnumerator DisplayDebtReminder(TimeOfDay Instance)
